6c9b7a998f74419cafa45d64c43ba46875c95542
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-09-23 13:42:54 +0200

    Naming

--- logical_history/geom.rb
+++ logical_history/geom.rb
@@ -93,8 +93,8 @@ module LogicalHistory
         elsif a_over_b.empty? || b_over_a.empty?
           # One subpart of the other
           union = r_geom_a.union(r_geom_b)
-          parts = exact_or_buffered_size_over_union(r_geom_a, r_geom_b, a_over_b, b_over_a, union) { |geom|
-            intersection.dimension == 1 ? T.unsafe(geom).length : T.unsafe(geom).area
+          parts = exact_or_buffered_size_over_union(r_geom_a, r_geom_b, a_over_b, b_over_a, union) { |geos|
+            intersection.dimension == 1 ? T.unsafe(geos).length : T.unsafe(geos).area
           }
           [0.0, parts[1], parts[2]]
         else
@@ -107,9 +107,9 @@ module LogicalHistory
             raise 'Non equal intersecting points, should never happen.'
           elsif dim_a == 1 && dim_b == 1 && dim_union == 1
             # Lines
-            exact_or_buffered_size_over_union(r_geom_a, r_geom_b, a_over_b, b_over_a, union) { |geom| T.unsafe(geom).length }
+            exact_or_buffered_size_over_union(r_geom_a, r_geom_b, a_over_b, b_over_a, union) { |geos| T.unsafe(geos).length }
           elsif dim_a == 2 && dim_b == 2 && dim_union == 2
-            exact_or_buffered_size_over_union(r_geom_a, r_geom_b, a_over_b, b_over_a, union) { |geom| T.unsafe(geom).area }
+            exact_or_buffered_size_over_union(r_geom_a, r_geom_b, a_over_b, b_over_a, union) { |geos| T.unsafe(geos).area }
           else
             raise 'Diff dimension geom should not happen.'
           end
