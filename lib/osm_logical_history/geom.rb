1c571c352dd986865172b011a8e6c1abe8ef2cb8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-07-18 10:35:10 +0200

    Fix geom_score

--- lib/osm_logical_history/geom.rb
+++ lib/osm_logical_history/geom.rb
@@ -96,7 +96,7 @@ module OSMLogicalHistory
 
       if r_geom_a.intersection(r_geom_b).dimension < r_geom_a.dimension
         # Excact distance give a lower dimension geom, use buffered distance
-        return [buffered_distance, a_over_b.empty? ? nil : a_over_b, b_over_a.empty? ? b_over_a : nil, 'intersection over union, distance to lower dimension']
+        return [buffered_distance, a_over_b.empty? ? nil : a_over_b, b_over_a.empty? ? nil : b_over_a, 'intersection over union, distance to lower dimension']
       end
 
       exact_a_over_b = r_geom_a - r_geom_b
